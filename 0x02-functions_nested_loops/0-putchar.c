7af40df1498df01ea37a0da06e5758f0a344e217
Pri3scilla <tunmiseomolegan3@gmail.com>
2022-09-13 13:53:17 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - Prints _putchar
- *
- * Return: Always 0 (Success)
- */
+* main - Prints _putchar
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
  _putchar('_');
@@ -14,7 +14,7 @@ int main(void)
  _putchar('h');
  _putchar('a');
  _putchar('r');
- _putchar('\n')
+ _putchar('\n');
  return (0);
 }
 
